edd3e814c13ec30a35986012b3e8a6397521f6b8
Charlie Crouse <charlietcrouse@gmail.com>
2019-03-03 17:20:33 -0500

    Add projects view to home page (#24)
    * Add projects relationship
    
    * Add projects to home page
    
    * Update tests for home

--- lamby/controllers/home.py
+++ lamby/controllers/home.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from lamby.models.project import Project
+
 home_blueprint = Blueprint('home', __name__)
 
 
 @home_blueprint.route('/')
 def index():
-    return render_template('home.jinja')
+    projects = Project.query.limit(15).all()
+    return render_template('home.jinja', projects=projects)
--- lamby/tests/controllers/test_home.py
+++ lamby/tests/controllers/test_home.py
@@ -1,6 +1,6 @@
-from lamby.tests.util import get_response_data_without_whitespace
+from lamby.tests.util import get_response_data
 
 
-def test_index(test_client):
+def test_index(test_client, test_db, scope='module'):
     res = test_client.get('/')
-    assert 'Hello,Anon!' in get_response_data_without_whitespace(res)
+    assert 'Popular Projects' in get_response_data(res)
